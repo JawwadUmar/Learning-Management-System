2f9bdf84567d1f3c8c238b77ea4edb671ee9b544
Jawwad Umar <jawwadumar99@gmail.com>
2026-08-22 20:55:34 +0530

    refactoring code

--- src/main/java/com/example/lms/controller/SignupController.java
+++ src/main/java/com/example/lms/controller/SignupController.java
@@ -2,7 +2,6 @@ package com.example.lms.controller;
 
 import com.example.lms.dto.ApiResponse;
 import com.example.lms.dto.SignupRequest;
-import com.example.lms.exception.UserAlreadyExistsException;
 import com.example.lms.service.SignupService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -10,9 +9,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("user")
 public class SignupController {
 
   private final SignupService signupService;
@@ -24,18 +25,7 @@ public class SignupController {
   @PostMapping(value = "/signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<ApiResponse<SignupRequest>> signup(
       @Valid @ModelAttribute SignupRequest signUpRequest) {
-    try {
-      signupService.handleSignup(signUpRequest);
-
-    } catch (UserAlreadyExistsException e) {
-      ApiResponse<SignupRequest> response = new ApiResponse<>(false, e.getMessage(), null);
-
-      return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
-    } catch (Exception e) {
-      ApiResponse<SignupRequest> response = new ApiResponse<>(false, e.getMessage(), null);
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-    }
-
+    signupService.handleSignup(signUpRequest);
     ApiResponse<SignupRequest> response =
         new ApiResponse<>(true, "Sign up successful", signUpRequest);
 
